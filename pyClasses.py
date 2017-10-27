27d2b807f3fe9a5b55c739fa77bdec7668ba5166
cfernandezmanci <32992320+cfernandezmanci@users.noreply.github.com>
2017-10-27 16:40:45 -0300

    Clase Principal + Asserts
    Se divide la clase principal (get DataFile + aplicación de Asserts) de la instancia de clases para llevar a cabo la aplicación de Facts

--- pyClasses.py
+++ pyClasses.py
@@ -3,7 +3,7 @@ import csv
 
 class Entidad():
     def __init__(self):
-        self.archivo = '/home/christian/tfiClips/empresas_1.csv'
+        self.archivo = ""
         self.data =""
         #"/home/christian/PycharmProjects/TFIClips/empresas_1.csv"
 
@@ -31,7 +31,7 @@ class Entidad():
         return temp
 
     def leeAssertTemplate(self,defTemp):
-        print "*** Vizualizacion Template Empresas"
+        print "*** Vizualizacion Template"
         print defTemp.PPForm()
 
 
@@ -43,91 +43,4 @@ class Entidad():
                 f1.Slots[valor] = row[indice]
                 indice =  indice +1
             f1.Assert()
-
-
-'''CARGA EMPRESAS '''
-
-emplista = Entidad()
-emplista.putArchivo('/home/christian/tfiClips/empresas_1.csv')
-emplista.leerArchivo()
-empre = emplista.getDatosArchivo()
-
-varClipTemplateNombre = """empresa"""
-varClipTemplateDatos = """
-    (slot eid (type INTEGER))
-    (slot nombre (type STRING))
-    (slot tipo (type STRING))
-    (slot categoria (type STRING))
-    (slot ciudad (type STRING))
-    (slot pais (type STRING))
-    (slot comercioext (type STRING))
-    (slot puntaje (type INTEGER))
- """
-varClipTemplateComentario = """Es Template Empresa"""
-
-varCamposTemp = ["eid","nombre","tipo","categoria","ciudad","pais","comercioext","puntaje"]
-
-tempEmpresa = emplista.addAssertTemplate(varClipTemplateNombre,varClipTemplateDatos,varClipTemplateComentario)
-emplista.leeAssertTemplate(tempEmpresa)
-emplista.putAssertTemplate(tempEmpresa,varCamposTemp,empre)
-
-#clips.PrintFacts()
-
-'''CARGA COMPRAS '''
-
-compralista = Entidad()
-compralista.putArchivo('/home/christian/tfiClips/servicios_1.csv')
-compralista.leerArchivo()
-comp = compralista.getDatosArchivo()
-
-varClipTemplateNombre = """compra"""
-varClipTemplateDatos = """
-    (slot sid (type INTEGER))
-    (slot eid (type INTEGER))
-    (slot nombre (type STRING))
- """
-varClipTemplateComentario = """Es Template Compras"""
-
-varCamposTemp = ["sid","eid","nombre"]
-
-tempCompra = compralista.addAssertTemplate(varClipTemplateNombre,varClipTemplateDatos,varClipTemplateComentario)
-compralista.leeAssertTemplate(tempCompra)
-compralista.putAssertTemplate(tempCompra,varCamposTemp,comp)
-
-
-
-
-'''CARGA VENTAS '''
-
-
-ventaLista = Entidad()
-ventaLista.putArchivo('/home/christian/tfiClips/productos_1.csv')
-ventaLista.leerArchivo()
-venta = ventaLista.getDatosArchivo()
-
-varClipTemplateNombre = """venta"""
-varClipTemplateDatos = """
-    (slot pid (type INTEGER))
-    (slot eid (type INTEGER))
-    (slot nombre (type STRING))
-    (slot precio (type STRING))
-    (slot moneda (type STRING))
-    (slot promocional (type STRING))
- """
-varClipTemplateComentario = """Es Template Ventas"""
-
-varCamposTemp = ["pid","eid","nombre","precio","moneda","promocional"]
-
-
-tempVenta = ventaLista.addAssertTemplate(varClipTemplateNombre,varClipTemplateDatos,varClipTemplateComentario)
-ventaLista.leeAssertTemplate(tempVenta)
-ventaLista.putAssertTemplate(tempVenta,varCamposTemp,venta)
-
-
-#clips.PrintFacts()
-
-
-clips.PrintFacts()
-clips.Clear()
-
 
---
+++ pyClassesAssert.py
@@ -0,0 +1,121 @@
+import clips
+import csv
+import pyClasses
+
+
+
+
+'''DEFINICIONES'''
+
+DIRECTORIO = "/home/fernandezc/PycharmProjects/TFIClips/"
+
+'''CARGA EMPRESAS '''
+
+emplista = pyClasses.Entidad()
+emplista.putArchivo(DIRECTORIO + "empresas_1.csv")
+emplista.leerArchivo()
+empre = emplista.getDatosArchivo()
+
+varClipTemplateNombre = """empresa"""
+varClipTemplateDatos = """
+    (slot eid (type INTEGER))
+    (slot nombre (type STRING))
+    (slot tipo (type STRING))
+    (slot categoria (type STRING))
+    (slot ciudad (type STRING))
+    (slot pais (type STRING))
+    (slot comercioext (type STRING))
+    (slot puntaje (type INTEGER))
+ """
+varClipTemplateComentario = """Es Template Empresa"""
+
+varCamposTemp = ["eid","nombre","tipo","categoria","ciudad","pais","comercioext","puntaje"]
+
+tempEmpresa = emplista.addAssertTemplate(varClipTemplateNombre,varClipTemplateDatos,varClipTemplateComentario)
+emplista.leeAssertTemplate(tempEmpresa)
+emplista.putAssertTemplate(tempEmpresa,varCamposTemp,empre)
+
+#clips.PrintFacts()
+
+'''CARGA COMPRAS '''
+
+compralista = pyClasses.Entidad()
+compralista.putArchivo(DIRECTORIO + "servicios_1.csv")
+compralista.leerArchivo()
+comp = compralista.getDatosArchivo()
+
+varClipTemplateNombre = """compra"""
+varClipTemplateDatos = """
+    (slot sid (type INTEGER))
+    (slot eid (type INTEGER))
+    (slot nombre (type STRING))
+ """
+varClipTemplateComentario = """Es Template Compras"""
+
+varCamposTemp = ["sid","eid","nombre"]
+
+tempCompra = compralista.addAssertTemplate(varClipTemplateNombre,varClipTemplateDatos,varClipTemplateComentario)
+compralista.leeAssertTemplate(tempCompra)
+compralista.putAssertTemplate(tempCompra,varCamposTemp,comp)
+
+
+
+'''CARGA VENTAS '''
+
+
+ventaLista = pyClasses.Entidad()
+ventaLista.putArchivo(DIRECTORIO + "productos_1.csv")
+ventaLista.leerArchivo()
+venta = ventaLista.getDatosArchivo()
+
+varClipTemplateNombre = """venta"""
+varClipTemplateDatos = """
+    (slot pid (type INTEGER))
+    (slot eid (type INTEGER))
+    (slot nombre (type STRING))
+    (slot precio (type STRING))
+    (slot moneda (type STRING))
+    (slot promocional (type STRING))
+ """
+varClipTemplateComentario = """Es Template Ventas"""
+
+varCamposTemp = ["pid","eid","nombre","precio","moneda","promocional"]
+
+
+tempVenta = ventaLista.addAssertTemplate(varClipTemplateNombre,varClipTemplateDatos,varClipTemplateComentario)
+ventaLista.leeAssertTemplate(tempVenta)
+ventaLista.putAssertTemplate(tempVenta,varCamposTemp,venta)
+
+
+'''CARGA ENLACES '''
+
+
+enlaceLista = pyClasses.Entidad()
+enlaceLista.putArchivo(DIRECTORIO + "enlaces_1.csv")
+enlaceLista.leerArchivo()
+enlace = enlaceLista.getDatosArchivo()
+
+varClipTemplateNombre = """enlace"""
+varClipTemplateDatos = """
+    (slot enlaceid (type INTEGER))
+    (slot fuente (type STRING))
+    (slot metodo (type STRING))
+    (slot valor (type STRING))
+    (slot sid (type INTEGER))
+    (slot pid (type INTEGER))
+    (slot epid (type INTEGER))
+    (slot esid (type INTEGER))
+ """
+varClipTemplateComentario = """Es Template Enlace"""
+
+varCamposTemp = ["enlaceid","fuente","metodo","valor","sid","pid","esid","epid"]
+
+
+tempEnlace = enlaceLista.addAssertTemplate(varClipTemplateNombre,varClipTemplateDatos,varClipTemplateComentario)
+enlaceLista.leeAssertTemplate(tempEnlace)
+enlaceLista.putAssertTemplate(tempEnlace,varCamposTemp,enlace)
+
+
+
+clips.PrintFacts()
+clips.Clear()
