915546d4bb80ec16eb942a3226f7a135ab0a1993
cfernandezmanci <cfernandezmancini@gmail.com>
2017-11-27 03:15:16 -0300

    Se divide en archivos cada uno de las reglas.

--- pyClassAssertPosClientes.py
+++ pyClassAssertPosClientes.py
@@ -6,7 +6,7 @@ import pyClassesAssertBasicos
 
 posClientesLimitrofesLista = pyClasses.Entidad()
 
-varClipTemplateNombre = """posclientesext"""
+varClipTemplateNombre = """posclientes"""
 varClipTemplateDatos = """
     (slot eid (type INTEGER))
     (slot nombre (type STRING))
@@ -19,7 +19,7 @@ varClipTemplateDatos = """
     (slot valor (type STRING))
     (slot enlaceid (type STRING))
  """
-varClipTemplateComentario = """Es Template Posibles Clientes Exteriores"""
+varClipTemplateComentario = """Es Template Posibles Clientes"""
 
 tempPosCliLoc = posClientesLimitrofesLista.addAssertTemplate(varClipTemplateNombre,varClipTemplateDatos,varClipTemplateComentario)
 posClientesLimitrofesLista.leeAssertTemplate(tempPosCliLoc)
@@ -37,9 +37,22 @@ clips.SendCommand("""
   (enlace (epid ?epid)(esid ?esid)(valor ?valor) (pid ?pid) (fuente ?fuente & "V")  (enlaceid ?enlaceid))
   (and(test(neq ?pais ?pais2)) (test(neq ?eid ?eid2)) (test(eq ?epid ?eid)) (test(eq ?esid ?eid2)))  
   =>
-  (assert(posclientesext (eid ?eid) (nombre ?nombre) (pais ?pais) (eid2 ?eid2) (nombre2 ?nombre2) (pais2 ?pais2) (venta ?pid) (valor ?valor)  (enlaceid ?enlaceid) ))
+  (assert(posclientes (eid ?eid) (nombre ?nombre) (pais ?pais) (eid2 ?eid2) (nombre2 ?nombre2) (pais2 ?pais2) (venta ?pid) (valor ?valor)  (enlaceid ?enlaceid) ))
  )
 """)
+
+
+clips.SendCommand("""
+(defrule posclienteslocales
+  (empresa (eid ?eid)(nombre ?nombre) (pais ?pais) )         
+  (empresa (eid ?eid2)(nombre ?nombre2) (pais ?pais2))
+  (enlace (epid ?epid)(esid ?esid)(valor ?valor) (pid ?pid) (fuente ?fuente & "V")  (enlaceid ?enlaceid))
+   (and(test(eq ?pais ?pais2)) (test(neq ?eid ?eid2)) (test(eq ?epid ?eid)) (test(eq ?esid ?eid2)) )  
+  =>
+  (assert(posclientes (eid ?eid) (nombre ?nombre) (pais ?pais) (eid2 ?eid2) (nombre2 ?nombre2) (pais2 ?pais2) (venta ?pid) (valor ?valor)  (enlaceid ?enlaceid) ))
+ )
+""")
+
 
 clips.PrintRules()
 clips.Run()
@@ -48,10 +61,14 @@ clips.Run()
 print "Resultado - Posibles Clientes Exteriores"
 lista = clips.FactList()
 for f in lista:
-    if f.Relation == 'posclientesext':
+    if f.Relation == 'posclientes':
         valorEnl = pyFormulas.formuValorEnlace(f.Slots["enlaceid"])
         if valorEnl == "EXCELENTE" or valorEnl == "MUY BUENO":
             tipoPais = pyFormulas.formuUbicacionEmpresaPais(f.Slots["pais"], f.Slots["pais2"])
+
+            if tipoPais == "LOCAL":
+                print f.Slots["eid"],f.Slots["nombre"],f.Slots["pais"],f.Slots["eid2"],f.Slots["nombre2"],f.Slots["pais2"],tipoPais,f.Slots["venta"]
+
 
             if tipoPais == "LIMITROFE":
                 print f.Slots["eid"],f.Slots["nombre"],f.Slots["pais"],f.Slots["eid2"],f.Slots["nombre2"],f.Slots["pais2"],tipoPais,f.Slots["venta"]
